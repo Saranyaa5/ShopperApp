2a4fe641dc5a322b92130fbb2c77579073492a34
SARANYA ALAGESAN <114178067+Saranyaa5@users.noreply.github.com>
2025-02-06 16:33:24 +0530

    Update ass1.java

--- ass1.java
+++ ass1.java
@@ -1,13 +1,14 @@
 import java.util.*;
 
 public class ass1 {
-public static void main(String []args) {
-	Scanner sc=new Scanner(System.in);
-	System.out.println("enter name: ");
-	String name=sc.nextLine();
-	System.out.println("enter age: ");
-	int age=sc.nextInt();
-	System.out.println("name: "+name +" age: " +age);
-	sc.close();
-	}
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.out.println("Usage: java ass1 <name> <age>");
+            return;
+        }
+        String name = args[0];
+        int age = Integer.parseInt(args[1]);
+
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
 }
