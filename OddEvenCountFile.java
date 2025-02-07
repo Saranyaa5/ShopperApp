faca88563f4463899c852ce84254edab54f09c17
SARANYA ALAGESAN <114178067+Saranyaa5@users.noreply.github.com>
2025-02-07 10:22:20 +0530

    Create OddEvenCountFile.java

---
+++ OddEvenCountFile.java
@@ -0,0 +1,30 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class OddEvenCountFromFile {
+    public static void main(String[] args) {
+        File file = new File("input.txt");
+        Scanner sc=new Scanner(System.in)
+        int size=sc.nextInt();
+        int arr[]=new arr[size];
+
+       for(int i=0;i<size;i++){
+         arr[i]=sc.nextInt();
+       }
+      
+    int odd=0;
+		int even=0;
+		for(int i=0;i<size;i++) {
+			if(arr[i]%2==0) {
+				even++;
+			}
+			else {
+				odd++;
+			}
+		}
+		System.out.printf("odd: %d even: %d",odd,even);
+    sc.close();
+        
+    }
+}
