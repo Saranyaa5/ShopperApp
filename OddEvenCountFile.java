3027a0f743ac9781d30f3fdea79bc72865a4db8b
SARANYA ALAGESAN <114178067+Saranyaa5@users.noreply.github.com>
2025-02-07 10:27:46 +0530

    Update OddEvenCountFile.java

--- OddEvenCountFile.java
+++ OddEvenCountFile.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class OddEvenCountFromFile {
     public static void main(String[] args) {
         File file = new File("input.txt");
-        Scanner sc=new Scanner(System.in);
+        Scanner sc=new Scanner(file);
         int size=sc.nextInt();
-        int arr[]=new arr[size];
+        int arr[]=new int[size];
 
        for(int i=0;i<size;i++){
          arr[i]=sc.nextInt();
