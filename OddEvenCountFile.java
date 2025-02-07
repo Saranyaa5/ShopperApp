d8a9c730c4d4741778441186e82808f502767b2d
SARANYA ALAGESAN <114178067+Saranyaa5@users.noreply.github.com>
2025-02-07 10:31:19 +0530

    Update OddEvenCountFile.java

--- OddEvenCountFile.java
+++ OddEvenCountFile.java
@@ -4,27 +4,29 @@ import java.util.Scanner;
 
 public class OddEvenCountFile {
     public static void main(String[] args) {
-        File file = new File("input.txt");
-        Scanner sc=new Scanner(file);
-        int size=sc.nextInt();
-        int arr[]=new int[size];
+        File file = new File("input.txt"); 
 
-       for(int i=0;i<size;i++){
-         arr[i]=sc.nextInt();
-       }
-      
-    int odd=0;
-		int even=0;
-		for(int i=0;i<size;i++) {
-			if(arr[i]%2==0) {
-				even++;
-			}
-			else {
-				odd++;
-			}
-		}
-		System.out.printf("odd: %d even: %d",odd,even);
-    sc.close();
-        
+        try (Scanner sc = new Scanner(file)) {
+            int size = sc.nextInt(); 
+            int arr[] = new int[size];
+
+            for (int i = 0; i < size; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            int odd = 0, even = 0;
+            for (int num : arr) {
+                if (num % 2 == 0) {
+                    even++;
+                } else {
+                    odd++;
+                }
+            }
+
+            System.out.printf("Odd: %d Even: %d%n", odd, even);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: input.txt not found. Please make sure the file exists.");
+        }
     }
 }
